f3f8f96d90f5acb7f19fabb3ae85d28fd4918ecd
mohcen-laalaoui <mohcenlaa@gmail.com>
2025-03-27 07:47:44 +0100

    improvements

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -50,8 +50,8 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDk75XOZBNmRHQUcCYh_QRLwLhY3cQclnk',
-    appId: '1:135042312110:android:caf57725d4ca81af140a3f',
+    apiKey: 'apiKey',
+    appId: 'ID',
     messagingSenderId: '135042312110',
     projectId: 'financialapp-cdb08',
     storageBucket: 'financialapp-cdb08.firebasestorage.app',
